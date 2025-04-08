d574b10a51754f9fe37551035f23c1e26ce6a203
Debora <ds336756@gmail.com>
2025-04-08 17:19:31 -0300

    Parte da estrutura do javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -20,3 +20,90 @@ mes_ext[8] = 'Setembro';
 mes_ext[9] = 'Outubro';
 mes_ext[10] = 'Novembro';
 mes_ext[11] = 'Dezembro';
+
+function Exibir() {
+    var d_ani = document.getElementById('dia').value;
+    var m_ani = document.getElementById('mes').value;
+    var a_ani = document.getElementById('ano').value;
+
+    if (isNaN(d_ani)) {
+        window.alert('Dígito inválido, digite números');
+
+    }
+
+    else if (d_ani == '') {
+        window.alert('O dia não pode ficar vazio')
+    }
+
+    else {
+        d_ani = parseInt(d_ani)
+
+        if ((d_ani < 1) || (d_ani > 31)) {
+
+            window.alert('o dia deve ser entre 1 e 31');
+        }
+
+    }
+
+    if (isNaN(m_ani)) {
+        window.alert('Dígito inválido, digite números');
+
+    }
+
+    else if (m_ani == '') {
+        window.alert('O mês não pode ficar vazio')
+    }
+
+    else {
+        d_ani = parseInt(m_ani)
+
+        if ((m_ani < 1) || (m_ani > 12)) {
+
+            window.alert('o mês deve ser entre 1 e 12');
+        }
+    }
+
+    if (isNaN(a_ani)) {
+        window.alert('Dígito inválido, digite números');
+
+    }
+
+    else if (a_ani == '') {
+        window.alert('O dia não pode ficar vazio')
+    }
+    else if (a_ani.length != 4) {
+        alert('O ano deve conter 4 digitos')
+    }
+
+    else {
+        d_ani = parseInt(d_ani)
+        m_ani = parseInt(m_ani)
+        a_ani = parseInt(a_ani)
+
+        var hoje = new Date();
+        var d_hoje = hoje.getDate();
+        var m_hoje = hoje.getMonth() + 1;
+        var a_hoje = hoje.getFullYear();
+
+        var idade = a_hoje - a_ani;
+        if (m_ani > m_hoje) {
+            idade--;
+        }else if((m_ani == m_hoje) && (d_ani > d_hoje)){
+            idade --;
+        }
+        document.getElementById('idade').value = idade;
+
+        hoje.getDate(d_ani);
+        hoje.setMonth(m_ani-1);
+        hoje.setFullYear(a_ani);
+
+        document.getElementById('sema').value = dia_semana[hoje.getDay()];
+        document.getElementById('mext').value = nome_mes[m_ani-1];
+
+        If (((d_ani >= 20 ) && (m_ani == 1)) || ((d_ani <= 18) && (m_ani == 2)))
+        document.images ['signo'].src = './';
+        else if (((d_ani >= 19) && (m_ani == 2)) || ((d_ani <= 20)  && (m_ani == 3)))
+            document.images['signo'].src = './';
+    }
+
+}
